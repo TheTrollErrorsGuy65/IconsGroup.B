0d5e3efc27d2bd5370c7d5c2551f485a958250b7
TheTrollErrorsGuy65 <35165895+TheTrollErrorsGuy65@users.noreply.github.com>
2018-10-02 15:04:07 +0200

    Project Files
    Uploaded project files.

---
+++ IconsGroup/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim desktopDirectory As String = "C:\Users\" + Environment.UserName + "\Desktop\"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            Directory.CreateDirectory(desktopDirectory + "Documents")
+            Directory.CreateDirectory(desktopDirectory + "Music")
+            Directory.CreateDirectory(desktopDirectory + "Photos")
+            Directory.CreateDirectory(desktopDirectory + "Videos")
+            Directory.CreateDirectory(desktopDirectory + "Other")
+            Directory.CreateDirectory(desktopDirectory + "Apps and Games")
+
+        Catch ex As Exception
+
+        End Try
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Try
+            For Each fileFound In My.Computer.FileSystem.GetFiles(desktopDirectory)
+                Dim documentfiles() As String
+                documentfiles = {"txt", "doc", "docx", "pdf", "odf", "xml", "rtf"}
+                For Each foundDocument In documentfiles
+                    If fileFound.EndsWith(foundDocument) Then
+                        File.Move(fileFound, desktopDirectory + "Documents\" + fileFound.Replace(desktopDirectory, ""))
+                    End If
+                Next
+                Dim photofiles() As String
+                photofiles = {"png", "jpg", "jpeg", "ico", "bmp", "psd", "tif", "gif"}
+                For Each foundPhoto In photofiles
+                    If fileFound.EndsWith(foundPhoto) Then
+                        File.Move(fileFound, desktopDirectory + "Photos\" + fileFound.Replace(desktopDirectory, ""))
+                    End If
+                Next
+                Dim musicfiles() As String
+                musicfiles = {"mp3", "wav", "cda", "wma", "flac", "ogg", "aiff", "m4a"}
+                For Each foundMusic In musicfiles
+                    If fileFound.EndsWith(foundMusic) Then
+                        File.Move(fileFound, desktopDirectory + "Music\" + fileFound.Replace(desktopDirectory, ""))
+                    End If
+                Next
+                Dim apps() As String
+                apps = {"exe", "EXE", "bat", "cmd", "lnk"}
+                For Each foundApps In apps
+                    If fileFound.EndsWith(foundApps) Then
+                        File.Move(fileFound, desktopDirectory + "Apps and Games\" + fileFound.Replace(desktopDirectory, ""))
+                    End If
+                Next
+                Dim vidfiles() As String
+                vidfiles = {"mp4", "avi", "wmv", "mov", "mkv"}
+                For Each foundVids In vidfiles
+                    If fileFound.EndsWith(foundVids) Then
+                        File.Move(fileFound, desktopDirectory + "Videos\" + fileFound.Replace(desktopDirectory, ""))
+                    End If
+                Next
+                File.Move(fileFound, desktopDirectory + "Other\" + fileFound.Replace(desktopDirectory, ""))
+            Next
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Form1_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.SizeChanged
+        Me.Width = 234
+        Me.Height = 85
+    End Sub
+End Class
---
+++ IconsGroup/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IconsGroup")> 
+<Assembly: AssemblyDescription("Grouping icons to stop desktop mess.")> 
+<Assembly: AssemblyCompany("TheTrollErrorsGuy65")> 
+<Assembly: AssemblyProduct("IconsGroup")> 
+<Assembly: AssemblyCopyright("Copyright © 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f21b7d60-2881-41b9-a03c-975779c5a990")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.1")> 
+<Assembly: AssemblyFileVersion("0.0.0.1")> 
